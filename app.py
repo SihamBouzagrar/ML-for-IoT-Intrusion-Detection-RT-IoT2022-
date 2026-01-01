061338de3429c1ae71d0f55955f1180074e9b306
Siham <sihambouzagrar@gmail.com>
2026-01-01 20:25:15 +0100

    Add .gitignore for Jupyter checkpoints

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+# ==============================
+#   HEADER & UI
+# ==============================
+st.image("http://www.ehtp.ac.ma/images/lo.png")
+st.write("""
+## MSDE6 : ML Course
+### IoT IDS Prediction App
+This app predicts **Attack Classes** based on IoT traffic data
+""")
+
+st.sidebar.image("https://cdn-icons-png.flaticon.com/512/1048/1048949.png", width=250)
+
+st.sidebar.write("### Load Excel File for Prediction")
+
+uploaded_file = st.sidebar.file_uploader("Upload your Excel file", type=["xlsx"])
+
+# ==============================
+#   LOAD MODEL + ENCODER
+# ==============================
+@st.cache_resource
+def load_pipeline():
+    pipeline = joblib.load("model.pkl")
+    label_encoder = joblib.load("target.pkl")
+    return pipeline, label_encoder
+
+pipeline, label_encoder = load_pipeline()
+
+# ==============================
+#   PROCESS FILE
+# ==============================
+if uploaded_file is not None:
+    # Read user Excel file
+    RT_IOT2022= pd.read_excel(uploaded_file)
+
+    st.subheader("📄 Input Data Preview")
+    st.write(RT_IOT2022.head())
+
+    # Predict
+    predictions = pipeline.predict(RT_IOT2022)
+
+    # Decode labels
+    decoded_predictions = label_encoder.inverse_transform(predictions)
+
+    # Probabilities
+    if hasattr(pipeline.named_steps["classifier"], "predict_proba"):
+        probs = pipeline.predict_proba(RT_IOT2022)
+        proba_RT_IOT2022 = pd.DataFrame(probs, columns=label_encoder.classes_)
+    else:
+        proba_df = None
+
+    # ==============================
+    #   DISPLAY RESULTS
+    # ==============================
+    st.subheader("🎯 Predicted Class")
+    st.write(decoded_predictions)
+
+    if proba_RT_IOT2022 is not None:
+        st.subheader("📊 Prediction Probabilities")
+        st.write(proba_RT_IOT2022)
+
+else:
+    st.write("➡️ Please upload an Excel (.xlsx) file to start prediction.")
